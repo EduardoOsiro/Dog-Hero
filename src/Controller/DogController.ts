c8083f8796f7c2f72ead9e378c3ee1a614bf1aea
EduardoOsiro <eduardoosiro1@gmail.com>
2022-07-27 16:59:48 -0300

    criação DogController

---
+++ src/Controller/DogController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { DogWalkDTO } from "../Model/DogWalkClass";
+
+
+export class DogController {
+
+     public async createWalk (req:Request, res: Response) {
+          const {date, duration, latitude, longitude. pets, start_time, end_time} = req.body
+          try {
+
+               const input: DogWalkDTO = {
+                    date,
+                    duration,
+                    latitude,
+                    longitude,
+                    pets,
+                    start_time,
+                    end_time
+               }
+
+               const response = await DogBusiness.createWalk(input)
+
+               res.status(200).send(response)
+          } catch (error:any) {
+               
+          }
+     }
+}
